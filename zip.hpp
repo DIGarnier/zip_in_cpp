b3c6b1d5d67b07b6029e3fab684dee917d1fb9f1
David Garnier <davidigarnier@gmail.com>
2019-10-25 18:22:10 -0400

    remove same size constraint

---
+++ zip.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <tuple>
+#include <functional>
+
+#include "specializations.hpp"
+
+namespace detail
+{
+    template<std::size_t I, typename ...List>
+	constexpr auto tuple_Nt_elements(const List& ...lists)
+	{
+		return std::tuple{std::ref(get<I>(lists))...};
+	}
+
+    template<typename... List, std::size_t... Is>
+    constexpr auto zip(std::index_sequence<Is...>, const List&... lists)
+    {
+        return std::array{tuple_Nt_elements<Is>(lists...)...};
+    }
+} // detail
+
+
+template<typename... List>
+constexpr auto zip(const List& ...lists)
+{
+    constexpr auto [min, max] = std::minmax({std::tuple_size_v<List>...});
+    return detail::zip(std::make_index_sequence<min>{}, lists...);
+}
